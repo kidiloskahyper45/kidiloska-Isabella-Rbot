90b25926cbce180cfdf182c49ca6606ce47b5459
rushichoksi <67454775+rushichoksi@users.noreply.github.com>
2020-07-27 14:52:52 +0530

    Update rss.py

--- tg_bot/modules/rss.py
+++ tg_bot/modules/rss.py
@@ -224,7 +224,7 @@ __help__ = """
 NOTE: In groups, only admins can add/remove RSS links to the group's subscription
 """
 
-__mod_name__ = "RSS Feed🌐"
+__mod_name__ = "RSS Feed"
 
 job = updater.job_queue
 
