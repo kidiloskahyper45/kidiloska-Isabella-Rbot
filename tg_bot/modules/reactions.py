0a4329ba344b72ba7415ad3c65ab5c3cedca4096
rushichoksi <67454775+rushichoksi@users.noreply.github.com>
2020-07-27 14:34:08 +0530

    Update reactions.py

--- tg_bot/modules/reactions.py
+++ tg_bot/modules/reactions.py
@@ -230,6 +230,6 @@ REACT_HANDLER = DisableAbleCommandHandler("react", react)
 
 dispatcher.add_handler(REACT_HANDLER)
 
-__mod_name__ = "React"
+__mod_name__ = "React😲"
 __command_list__ = ["react"]
 __handlers__ = [REACT_HANDLER]
